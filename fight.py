cfec62cf8d6e226c8ffb0e76a0dee69b239c93a0
samueller9 <60405236+samueller9@users.noreply.github.com>
2021-09-27 17:52:40 -0700

    added abilities and armor

--- fight.py
+++ fight.py
@@ -4,7 +4,8 @@ import random
 
 def fight(self, opponent):
     ''' Current Hero will take turns fighting the opponent hero passed in.
-    '''    
+    '''
+
     hero1 = hero("Wonder Woman")
     hero2 = hero("Dumbledore")
 
--- hero.py
+++ hero.py
@@ -12,20 +12,8 @@ class Hero:
         self.starting_health = starting_health
         self.current_health = starting_health
 
-    def fight(self, opponent):
-        ''' Current Hero will take turns fighting the opponent hero passed in.
-        '''
-
 
 if __name__ == "__main__":
     my_hero = Hero("Grace Hopper", 200)
-    # print(my_hero.name)
-    # print(my_hero.current_health)
-
-if __name__ == "__main__":
-    # If you run this file from the terminal
-    # this block is executed.
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
-
-    hero1.fight(hero2)
+    print(my_hero.name)
+    print(my_hero.current_health)
